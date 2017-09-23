8e28bae68a77dc6a9fa11b86a3384a729b472fa1
Daniel <slayerdog4@gmail.com>
2017-09-23 09:26:45 +0300

    Add files via upload
    Changelog:
    *reading from .csv file;
    *filling COMPANY class;
    //TODO//
    writing into .json and .xml file;
    --NOTE--
    CSV.java is not using in this version of program;

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,59 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Main {
+
+    private static final String DELIM = ";";
+
     public static void main(String[] args) throws IOException {
-        List<List<String>> rows = new ArrayList<>();
-        String csvFile = "tq.csv";
-        try (InputStream in = new FileInputStream(csvFile)) {
-            CSV csv = new CSV(true, ';', in);
-            List<String> columnNames = null;
-            if (csv.hasNext()) {
-                columnNames = new ArrayList<String>(csv.next());
-            }
-            while (csv.hasNext()) {
-                List<String> fields = new ArrayList<String>(csv.next());
-                rows.add(fields);
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader("src/book.csv"));
+            List<Company> companyList = new ArrayList<>();
+            String line = "";
+            in.readLine();
+            while ((line = in.readLine()) != null) {
+                String[] companyDetails = line.split(DELIM);
+
+                if(companyDetails.length > 0) {
+                    Company company = new Company(
+                            companyDetails[0],
+                            companyDetails[1],
+                            companyDetails[2],
+                            companyDetails[3],
+                            companyDetails[4],
+                            Integer.parseInt(companyDetails[5]),
+                            companyDetails[6],
+                            companyDetails[7],
+                            companyDetails[8],
+                            companyDetails[9],
+                            companyDetails[10],
+                            companyDetails[11]
+                            );
+                    companyList.add(company);
+                }
+                for (Company e : companyList) {
+                    System.out.println(e.getName() + " " + e.getShortTitle() + " " + e.getDateUpdate() + " " + e.getAddress() + " " + e.getDateFoundation()
+                    + " " + e.getCountEmployees() + " " + e.getAuditor() + " " + e.getPhone() + " " + e.getEmail() + " " + e.getBranch() + " " + e.getActivity()
+                    + " " + e.getLink());
+                }
             }
         }
         catch (IOException e) {
-            e.getMessage();
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                in.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
     }
